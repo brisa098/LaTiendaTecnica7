1aa5f4d0c7b738ca89e9f332cf0d864ab0c21ee3
leonbeltranmazzaferri <leonbeltram07@gmail.com>
2025-06-25 20:27:35 -0300

    clase try , catch y eso

--- js/obtenerFetch.js
+++ js/obtenerFetch.js
@@ -1,16 +1,24 @@
 // promesa sincronica
-fetch('https://fakestoreapi.com/products') //endpoint
-  .then(response => response.json())
-  .then(data => console.log(data));
+// fetch('https://fakestoreapi.com/products') //endpoint
+//   .then(response => response.json())
+//   .then(data => console.log(data));
   
-
   //promesa asincronica
-  const endpoint = './data/data.json'
-
-  const obtenerProd = async() => {
-
-    response = awaitFetch(endpoint)
-    response = response.json()
+const endpoint = './data/data.json'
+const obtenerProd = async() => {
+    response = await fetch('./data/data.json')
+    response = await response.json()
     console.log(response)
-
   }
+  //promesa asincronica2
+  // const obtenerProd2 = async() => {
+  // try{
+  // response = awaitFetch(endpoint)
+  //   response = awaitresponse.json()
+  //   console.log(response)
+  // }
+  // catch{
+  //   console.log("error")
+  //  } 
+
+  // }
